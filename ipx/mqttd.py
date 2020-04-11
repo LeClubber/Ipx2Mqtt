648ac76344a28de1582aab3ccdffe1bceb55f2f3
LeClubber <braud.jeremy@gmail.com>
2020-04-11 06:37:16 +0200

    Suppression dict d'init

--- ipx/mqttd.py
+++ ipx/mqttd.py
@@ -11,8 +11,6 @@ from threading import Thread
 class Mqtt(Thread):
     """ Thread chargé de la connexion au broker MQTT """
     
-    listConfig = dict()
-
     def __init__(self, mqttHost, mqttPort, mqttTopic, ipxHost, ipxApiKey, ipxLogin, ipxPassword):
         Thread.__init__(self)
         self.mqttHost = mqttHost
@@ -31,11 +29,6 @@ class Mqtt(Thread):
         client.subscribe(topic)
         topic = self.mqttTopic + "/switch/+/set"
         client.subscribe(topic)
-        # On s'abonne aux config pour l'initialisation
-        topic = self.mqttTopic + "/light/+/config"
-        client.subscribe(topic)
-        topic = self.mqttTopic + "/switch/+/config"
-        client.subscribe(topic)
 
     def on_message(self, client, userdata, msg):
         """ Traitement du message recu """
@@ -43,43 +36,37 @@ class Mqtt(Thread):
         topic = str(msg.topic)
         payload = str(msg.payload, encoding="utf-8")
 
-        if topic.endswith("config"):
-            if payload:
-                Mqtt.listConfig[topic] = payload
-            else:
-                Mqtt.listConfig.pop(topic)
+        relay = topic.replace(self.mqttTopic, '').replace('/light/', '').replace('/switch/', '').replace('/set', '')
+        json_data = json.loads(payload)
+        if relay.lower().startswith("d"):
+            # Gestion des XDimmer
+            dimmer = relay[1:2]
+            channel = relay[3:]
+            urlIpx += "/user/api.cgi?SetDim=" + dimmer + "&DimCha=" + channel + "&DimValue="
+            if "OFF" == json_data['state']:
+                urlIpx += "0"
+            elif "ON" == json_data['state']:
+                brightness = ""
+                if "brightness" in json_data:
+                    # On change d'échelle de 255 à 100
+                    brightness = str(int(int(json_data['brightness'])/2.55))
+                else:
+                    # Si la valeur n'est pas renseignée on prend la dernière en statut
+                    req = requests.get("http://" + self.ipxHost + "/api/xdevices.json?key=" + self.ipxApiKey + "&Get=G")
+                    jsonStatus = json.loads(req.text)
+                    numStatus = int(dimmer)*int(channel)
+                    brightness = str(jsonStatus['G' + str(numStatus)]['Valeur'])
+                urlIpx += brightness
         else:
-            relay = topic.replace(self.mqttTopic, '').replace('/light/', '').replace('/switch/', '').replace('/set', '')
-            json_data = json.loads(payload)
-            if relay.lower().startswith("d"):
-                # Gestion des XDimmer
-                dimmer = relay[1:2]
-                channel = relay[3:]
-                urlIpx += "/user/api.cgi?SetDim=" + dimmer + "&DimCha=" + channel + "&DimValue="
-                if "OFF" == json_data['state']:
-                    urlIpx += "0"
-                elif "ON" == json_data['state']:
-                    brightness = ""
-                    if "brightness" in json_data:
-                        # On change d'échelle de 255 à 100
-                        brightness = str(int(int(json_data['brightness'])/2.55))
-                    else:
-                        # Si la valeur n'est pas renseignée on prend la dernière en statut
-                        req = requests.get("http://" + self.ipxHost + "/api/xdevices.json?key=" + self.ipxApiKey + "&Get=G")
-                        jsonStatus = json.loads(req.text)
-                        numStatus = int(dimmer)*int(channel)
-                        brightness = str(jsonStatus['G' + str(numStatus)]['Valeur'])
-                    urlIpx += brightness
-            else:
-                # Gestion des relais IPX et X8R
-                urlIpx += '/api/xdevices.json?key=' + self.ipxApiKey + '&'
-                if "ON" == json_data['state']:
-                    urlIpx += 'SetR='
-                elif "OFF" == json_data['state']:
-                    urlIpx += 'ClearR='
-                urlIpx += relay.replace('r', '')
-            # Reqete de commande à l'IPX
-            requests.get(urlIpx, auth=(self.ipxLogin, self.ipxPassword))
+            # Gestion des relais IPX et X8R
+            urlIpx += '/api/xdevices.json?key=' + self.ipxApiKey + '&'
+            if "ON" == json_data['state']:
+                urlIpx += 'SetR='
+            elif "OFF" == json_data['state']:
+                urlIpx += 'ClearR='
+            urlIpx += relay.replace('r', '')
+        # Reqete de commande à l'IPX
+        requests.get(urlIpx, auth=(self.ipxLogin, self.ipxPassword))
 
     def run(self):
         """ Démarrage du service MQTT """
